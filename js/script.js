dd656047ffc24e3f62e8be6a05d14c5fc3e1ea1e
taratkevich-ann <taratkevich.ann@ukr.net>
2020-09-05 14:38:10 +0300

    create statistic block, add styles for it

--- js/script.js
+++ js/script.js
@@ -3,3 +3,25 @@ let navBar = document.querySelector('.nav-bar');
 toggleButton.addEventListener('click', function () {
     navBar.classList.toggle('toggle');
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter("count1", 600, 748, 1500);
+    counter("count2", 0, 24, 2000);
+    counter("count3", 3500, 3854, 1000);
+    counter("count4", 0, 14, 2000);
+});
+
